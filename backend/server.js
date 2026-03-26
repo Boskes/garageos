e17832d9870a8ca092522fa35a3c6e809eed537b
Boskes <jurgen.bosmans@bosis.be>
2026-03-26 07:47:24 +0100

    refactor: split into backend/ and frontend/ structure

--- backend/server.js
+++ backend/server.js
@@ -11,10 +11,10 @@ const PORT = 3002;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, '../frontend')));
 
 app.get('/', (_req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, '../frontend', 'index.html'));
 });
 
 app.listen(PORT, () => {
